19e5e5de1d5d5ed31f6d343321d6ff7d787caabc
DavidVentura <davidventura27@gmail.com>
2023-10-04 16:26:10 +0200

    working insert and selects

--- django_prisma/base.py
+++ django_prisma/base.py
@@ -1,21 +1,41 @@
+import json
+
+import requests
+import urllib3
+
+urllib3.disable_warnings()
+
+from django.db.backends.postgresql.features import DatabaseFeatures
 from django.db.backends.base.creation import BaseDatabaseCreation
 from django.db.backends.base.client import BaseDatabaseClient
 from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.backends.base.operations import BaseDatabaseOperations
 from django.db.backends.base.introspection import BaseDatabaseIntrospection
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
-class PrismaDatabaseFeatures:
-    def __init__(self, wrapper):
-        self.wrapper = wrapper
+
+class PrismaDatabaseFeatures(DatabaseFeatures):
+    pass
+
 
 class PrismaDatabaseOperations(BaseDatabaseOperations):
     def __init__(self, wrapper):
         self.wrapper = wrapper
         self.compiler_module = "django_prisma.compiler"
+        super().__init__(wrapper)
+
+    def quote_name(self, name):
+        return f'"{name}"'
+
+    def bulk_insert_sql(self, fields, placeholder_rows):
+        print("bulk", fields, placeholder_rows)
+        return ""
+
 
 class PrismaDatabaseClient(BaseDatabaseClient):
     def __init__(self, wrapper):
         self.wrapper = wrapper
+
 
 class PrismaDatabaseIntrospection(BaseDatabaseIntrospection):
     def __init__(self, wrapper):
@@ -24,15 +44,22 @@ class PrismaDatabaseIntrospection(BaseDatabaseIntrospection):
     def get_table_list(self, cursor):
         return []
 
+
 class PrismaDatabaseCreation(BaseDatabaseCreation):
     pass
 
+
 class Error(Exception):
     pass
+
+
 class DatabaseError(Error):
     pass
+
+
 class DataError(DatabaseError):
     pass
+
 
 class PrismaDatabase:
     DataError = DataError
@@ -46,23 +73,42 @@ class PrismaDatabase:
     InterfaceError = Error
     Error = Error
 
+
+url = (
+    "https://accelerate.prisma-data.net/5.1.1/25765d5ea75691a914d604c337b58f4266f172207c9572dbbce34e59ef38ee8b/graphql"
+)
+
+
 class Cursor:
+    def __init__(self, token):
+        self.token = token
+
+    def execute(self, other, other2=None):
+        key = other.statement["action"] + other.statement["modelName"]  # findMany User
+        data = json.dumps(other.statement)
+        headers = {"Connection": "keep-alive", "Authorization": f"Bearer {self.token}"}
+        r = requests.post(url, headers=headers, verify=False, data=data)
+        _json = r.json()
+        for error in _json.get("errors", []):
+            ufe = error["user_facing_error"]
+            if ufe["error_code"] == "P2002":
+                raise PrismaDatabase.IntegrityError(ufe["message"])
+
+        result = r.json()["data"][key]
+        if isinstance(result, list):
+            return [[[r[colname] for colname in other.field_names] for r in result]]
+        return [[[result[colname] for colname in other.field_names]]]
+
     def close(self):
         pass
 
-def complain(*args, **kwargs):
-    raise ImproperlyConfigured(
-        "settings.DATABASES is improperly configured. "
-        "Please supply the ENGINE value. Check "
-        "settings documentation for more details."
-    )
 
-
-def ignore(*args, **kwargs):
+class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     pass
 
+
 class PrismaDatabaseWrapper(BaseDatabaseWrapper):
-    vendor = 'Prisma'
+    vendor = "Prisma"
     Database = PrismaDatabase
 
     client_class = PrismaDatabaseClient
@@ -70,6 +116,7 @@ class PrismaDatabaseWrapper(BaseDatabaseWrapper):
     features_class = PrismaDatabaseFeatures
     introspection_class = PrismaDatabaseIntrospection
     ops_class = PrismaDatabaseOperations
+    SchemaEditorClass = DatabaseSchemaEditor
 
     operators = {}
 
@@ -77,8 +124,7 @@ class PrismaDatabaseWrapper(BaseDatabaseWrapper):
         super().__init__(*args, **kwargs)
 
     def create_cursor(self, name=None):
-        print("cursor")
-        return Cursor()
+        return Cursor(self.settings_dict["TOKEN"])
 
     def is_usable(self):
         return True
@@ -88,5 +134,6 @@ class PrismaDatabaseWrapper(BaseDatabaseWrapper):
 
     def connect(self):
         print("called connect")
+
 
 DatabaseWrapper = PrismaDatabaseWrapper
--- django_prisma/compiler.py
+++ django_prisma/compiler.py
@@ -1,3 +1,7 @@
+from typing import Any
+from django.db.models.query import Field
+from django.db.models.expressions import Col
+from django.db.models.sql.constants import MULTI, CURSOR, SINGLE
 from django.db.models.sql.compiler import (
     SQLAggregateCompiler as BaseSQLAggregateCompiler,
     SQLCompiler as BaseSQLCompiler,
@@ -5,31 +9,140 @@ from django.db.models.sql.compiler import (
     SQLInsertCompiler as BaseSQLInsertCompiler,
     SQLUpdateCompiler as BaseSQLUpdateCompiler,
 )
+from django.db.models.sql.where import WhereNode, AND, tree
+from django.db.models.lookups import Exact
+
+
+class InsertStatement:
+    def __init__(self, model: str, field_names: list[Field], values: list):
+        self.model = model
+        self.field_names = [f.name for f in field_names]
+        self.field_values = values
+
+    @property
+    def statement(self):
+        data = dict(zip(self.field_names, self.field_values))
+        statement = {
+            "modelName": self.model,
+            "action": "createOne",
+            "query": {"arguments": {"data": data}, "selection": {"$composites": True, "$scalars": True}},
+        }
+        return statement
+
+    def __mod__(self, field_values):
+        self.field_values = field_values()
+        return self.statement
+
+
+def node_to_dict(n: tree.Node):
+    match n:
+        case Exact():
+            return {n.lhs.field.name: n.rhs}
+        case WhereNode():
+            return where_to_dict(n)
+    assert False
+
+
+def where_to_dict(w: WhereNode):
+    ret = {}
+    if w.connector == AND:
+        for c in w.children:
+            ret.update(node_to_dict(c))
+    return ret
+
+
+class SelectStatement:
+    def __init__(self, model: str, cols: list[Col], where: WhereNode):
+        self.model = model
+        self.field_names = [f.field.name for f in cols]
+        self.where = where
+        _where = where_to_dict(where)
+        self.statement = {
+            "modelName": "User",
+            "action": "findMany",
+            "query": {
+                "arguments": {
+                    "skip": 0,
+                    "take": 4,
+                    "where": _where,
+                },
+                "selection": {"$composites": True, "$scalars": True},
+            },
+        }
+
+    def query(self) -> dict[str, Any]:
+        return self.statement
 
 
 class SQLCompiler(BaseSQLCompiler):
+    def __init__(self, query, connection, using, elide_empty=True):
+        super().__init__(query, connection, using, elide_empty)
+
+    def as_sql(self):
+        # i think only sql.InsertQuery.__str__ calls as_sql
+        raise ValueError("somebody still calls as_sql")
+
+    def executable(self):
+        # pre_sql_setup mutates self and populates `self.select`
+        extra_select, order_by, group_by = self.pre_sql_setup(with_col_aliases=False)
+        opts = self.query.get_meta()
+        fields = [f[0] for f in self.select]
+        st = SelectStatement(opts.db_table, fields, self.where)
+        return st
+
+    def field_as_sql(self, field, val):
+        raise ValueError()
+
+    def execute_sql(self, result_type=MULTI, chunked_fetch=False, chunk_size=1024):
+        q = self.executable()
+        c = self.connection.cursor()
+        res = c.execute(q)
+        if res and result_type == SINGLE:
+            assert len(res) == 1
+            return res[0]
+        return res
+
+    def assemble_as_sql(self, fields, value_rows):
+        raise ValueError
+
+
+class SQLInsertCompiler(SQLCompiler, BaseSQLInsertCompiler):
+    """A wrapper class for compatibility with Django specifications."""
+
+    def executable(self):
+        opts = self.query.get_meta()
+        fields = self.query.fields or [opts.pk]
+        assert len(self.query.objs) == 1
+        values = [
+            [self.prepare_value(field, self.pre_save_val(field, obj)) for field in fields] for obj in self.query.objs
+        ]
+        values = values[0]
+        st = InsertStatement(opts.db_table, fields, values)
+        return [st, values]
+
+    def execute_sql(self, returning_fields=None):
+        print("retfields", returning_fields)
+        with self.connection.cursor() as cursor:
+            st, _ = self.executable()
+            return cursor.execute(st)
+        # this is supposed to apply converters if returning
+
     pass
 
 
-class SQLInsertCompiler(BaseSQLInsertCompiler, SQLCompiler):
+class SQLDeleteCompiler(SQLCompiler, BaseSQLDeleteCompiler):
     """A wrapper class for compatibility with Django specifications."""
 
     pass
 
 
-class SQLDeleteCompiler(BaseSQLDeleteCompiler, SQLCompiler):
+class SQLUpdateCompiler(SQLCompiler, BaseSQLUpdateCompiler):
     """A wrapper class for compatibility with Django specifications."""
 
     pass
 
 
-class SQLUpdateCompiler(BaseSQLUpdateCompiler, SQLCompiler):
-    """A wrapper class for compatibility with Django specifications."""
-
-    pass
-
-
-class SQLAggregateCompiler(BaseSQLAggregateCompiler, SQLCompiler):
+class SQLAggregateCompiler(SQLCompiler, BaseSQLAggregateCompiler):
     """A wrapper class for compatibility with Django specifications."""
 
     pass
